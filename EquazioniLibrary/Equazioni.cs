3d3a563c6bb407aba21795ff4ade2dd1cf358580
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-03-07 09:20:50 +0100

    Aggiunto metodo IsDeterminated

--- EquazioniLibrary/Equazioni.cs
+++ EquazioniLibrary/Equazioni.cs
@@ -8,6 +8,13 @@ namespace EquazioniLibrary
 {
     public class Equazioni
     {
-        
+        static bool IsDetermined(double a, double b)
+        {
+            bool ret = false;
+
+            if (a != 0 && b != 0) ret = true;
+
+            return ret;
+        }
     }
 }
